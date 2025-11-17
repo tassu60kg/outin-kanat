fa63979f59b7ba55f0a1368dbfff9416b6431d6a
Laura <immonen.laura.maria@gmail.com>
2025-11-17 12:29:55 +0200

    schema, luo taulu db_references

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bib_references (
+    id SERIAL PRIMARY KEY,
+    cite_key TEXT UNIQUE NOT NULL,
+    type TEXT NOT NULL,
+    title TEXT NOT NULL,
+    author TEXT,
+    year INTEGER,
+    booktitle TEXT,
+    journal TEXT,
+    publisher TEXT,
+    volume TEXT,
+    pages TEXT
+);

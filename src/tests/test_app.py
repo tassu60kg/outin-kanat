663d2a3b5da3ddbb923cb2e776d9ce78ff3dbf5c
Ronja <ronja.sonninen@helsinki.fi>
2025-11-25 13:54:38 +0200

    refaktoroitu

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -5,9 +5,9 @@ class TestValidation(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_vuosi_on_positiivinen_integer(self):
+    def test_year_is_positive_integer(self):
         validate_year(2020)
 
-    def test_vuosi_on_negatiivinen_integer(self):
+    def test_year_is_negative_integer(self):
         with self.assertRaises(UserInputError):
             validate_year(-5)

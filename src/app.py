e3a5967f4fb3b18773d0ad8742aac31861cc2053
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-11-25 13:26:42 +0200

    Lisättiin muokkaa viite ominaisuus.
    Co-authored-by: Ronja Sonninen <ronja.sonninen@helsinki.fi>

--- src/app.py
+++ src/app.py
@@ -28,11 +28,13 @@ def submit():
         "pages": request.form.get("pages"),
     }
 
-    validate_year(int(data["year"]))
-
-    repositories.reference_repositories.add_reference(**data)
-
+    if "send" in request.form:
+        validate_year(int(data["year"]))
+        repositories.reference_repositories.add_reference(**data)
+        return redirect("/")
+    
     return redirect("/")
+
 
 @app.route("/remove_reference/<int:reference_id>", methods=["GET", "POST"])
 def remove_reference(reference_id):
@@ -48,3 +50,31 @@ def remove_reference(reference_id):
             return redirect("/")
 
     return redirect("/")
+
+@app.route("/update_reference/<int:reference_id>", methods=["GET", "POST"])
+def update_reference(reference_id):
+    reference = repositories.reference_repositories.get_reference_by_id(reference_id)
+
+    if request.method == "GET":
+        return render_template("update_reference.html", reference=reference)
+
+    if request.method == "POST":
+        data = {
+        "cite_key": request.form.get("cite_key"),
+        "type": request.form.get("type"),
+        "author": request.form.get("author"),
+        "title": request.form.get("title"),
+        "year": request.form.get("year"),
+        "publisher": request.form.get("publisher"),
+        "ISBN": request.form.get("ISBN"),
+        "journal": request.form.get("journal"),
+        "booktitle": request.form.get("booktitle"),
+        "volume": request.form.get("volume"),
+        "pages": request.form.get("pages"),
+        }
+        if "update" in request.form:
+            repositories.reference_repositories.update_reference(**data)
+            flash("Reference updated successfully.")
+            return redirect("/")
+        
+        return redirect("/")
--- src/repositories/reference_repositories.py
+++ src/repositories/reference_repositories.py
@@ -26,3 +26,9 @@ def remove_reference(reference_id):
     sql = "DELETE FROM bib_references WHERE id = :id"
     db.session.execute(text(sql), {"id": reference_id})
     db.session.commit()
+
+def update_reference(**data):
+    sql = """UPDATE bib_references SET cite_key=:cite_key, type=:type, author=:author, title=:title, year=:year,
+         publisher=:publisher, ISBN=:ISBN, journal=:journal, booktitle=:booktitle, volume=:volume, pages=:pages"""
+    db.session.execute(text(sql), data)
+    db.session.commit()
